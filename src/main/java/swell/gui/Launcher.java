25430a25c0c9b82ca4e1baf3e6638fd8a99b9c82
AmyLiiiii <li_zhaohan@u.nus.edu>
2026-09-01 18:08:46 +0800

    Add JavaFX application launcher

---
+++ src/main/java/swell/gui/Launcher.java
@@ -0,0 +1,17 @@
+package swell.gui;
+
+import javafx.application.Application;
+
+/**
+ * Launches the Swell JavaFX application.
+ */
+public class Launcher {
+    /**
+     * Starts the JavaFX runtime.
+     *
+     * @param args command line arguments supplied to the program.
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
