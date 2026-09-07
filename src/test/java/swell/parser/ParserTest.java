325fbb1485d4940be0baea2928540ddbb1502c6a
AmyLiiiii <li_zhaohan@u.nus.edu>
2026-09-07 19:13:40 +0800

    Parse tags from task descriptions

--- src/test/java/swell/parser/ParserTest.java
+++ src/test/java/swell/parser/ParserTest.java
@@ -2,7 +2,10 @@ package swell.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +25,15 @@ public class ParserTest {
     }
 
     @Test
+    public void getTask_todoCommandWithTags_returnsTaggedTodoTask() throws SwellException {
+        Task task = Parser.getTask("todo email Alice #Acme #followup");
+
+        assertInstanceOf(Todo.class, task);
+        assertEquals("[T][ ] email Alice #Acme #followup", task.toString());
+        assertIterableEquals(List.of("Acme", "followup"), task.getTags());
+    }
+
+    @Test
     public void getTask_deadlineCommandWithIsoDate_returnsFormattedDeadline() throws SwellException {
         Task task = Parser.getTask("deadline return book /by 2019-10-15");
 
@@ -30,11 +42,30 @@ public class ParserTest {
     }
 
     @Test
+    public void getTask_deadlineCommandWithTag_returnsTaggedDeadlineTask() throws SwellException {
+        Task task = Parser.getTask("deadline submit resume #Acme /by 2019-10-15");
+
+        assertInstanceOf(Deadline.class, task);
+        assertEquals("[D][ ] submit resume #Acme (by: Oct 15 2019)", task.toString());
+        assertIterableEquals(List.of("Acme"), task.getTags());
+    }
+
+    @Test
     public void getTask_eventCommand_returnsEventTask() throws SwellException {
         Task task = Parser.getTask("event meeting /from Mon /to Tue");
 
         assertInstanceOf(Event.class, task);
         assertEquals("[E][ ] meeting (from: Mon to: Tue)", task.toString());
+    }
+
+    @Test
+    public void getTagKeyword_tagWithHash_returnsTagWithoutHash() throws SwellException {
+        assertEquals("followup", Parser.getTagKeyword("findtag #followup"));
+    }
+
+    @Test
+    public void getTagKeyword_emptyHashTag_throwsSwellException() {
+        assertThrows(SwellException.class, () -> Parser.getTagKeyword("findtag #"));
     }
 
     @Test
